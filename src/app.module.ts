ce361458e7ee1d67aa8e487ae2b5857db18d1148
Mikhail Bushuev <mixabush@gmail.com>
2025-06-04 15:03:46 +0200

    Task 2...

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from './schedule/schedule.module';
 import { RoomModule } from './room/room.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NpmModule } from './install/npm/npm.module';
 
 @Module({
-  imports: [ScheduleModule, RoomModule],
+  imports: [
+    ScheduleModule,
+    RoomModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/test'),
+    NpmModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
